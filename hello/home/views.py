d3fef568308dfda88a6fe9b14d4b47ba94348758
pragyan69 <prajyanqw@gmail.com>
2023-05-03 00:32:59 +0530

    second

--- hello/home/views.py
+++ hello/home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render , HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("this is home page")
+
+def about(request):
+    return HttpResponse("this is a about page")
+
+def services(request):
+    return HttpResponse("thisi is a services page")
