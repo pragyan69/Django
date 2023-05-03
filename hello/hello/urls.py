2801f6af10b77ccb00562eeb1c0214a3b5421c2e
pragyan69 <prajyanqw@gmail.com>
2023-05-03 11:54:57 +0530

    adding the urls

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -2,7 +2,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from home import views
-
+admin.site.site_header = "prantu boss of the world"
+admin.site.site_title = "prantu boss Portal"
+admin.site.index_title = "welcome to the boss Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('home.urls'))
