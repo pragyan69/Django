d1861d9af5eb0b756cea64df317a0787f1a4349a
pragyan69 <prajyanqw@gmail.com>
2023-05-03 10:08:39 +0530

    first

--- hello/home/views.py
+++ hello/home/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render , HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is home page")
+   context = {
+       'variable':"this is sent"
+   }
+   return render(request , 'index.html',context)
+   #  return HttpResponse("this is home page")
 
 def about(request):
     return HttpResponse("this is a about page")
